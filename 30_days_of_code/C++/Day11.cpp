80473194d4143641e4958dd62a73358356f22ff7
priyanshumay <tiwarimay2002@gmail.com>
2020-04-10 23:32:38 +0530

    fixed

--- 30_days_of_code/C++/Day11.cpp
+++ 30_days_of_code/C++/Day11.cpp
@@ -1,24 +1,34 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
+
 int main()
 {
-    int i, j, a[6][6], sum = 0, max = 0, size = 6;
-    for (i = 0; i < 6; i++)
+    vector<vector<int>> arr(6, vector<int>(6));
+    for (int i = 0; i < 6; i++)
     {
-        for (j = 0; j < 6; j++)
+        for (int j = 0; j < 6; j++)
         {
-            cin >> a[i][j];
+            cin >> arr[i][j];
         }
     }
-    for (i = 0; i < (size - 2); i++)
+
+    int max = -9 * 7;
+    for (int i = 0; i < 6; i++)
     {
-        for (j = 0; j < (size - 2); j++)
+        for (int j = 0; j < 6; j++)
         {
-            sum = a[i][j] + a[i][j + 1] + a[i][j + 2] + a[i + 1][j + 1] + a[i + 2][j] + a[i + 2][j + 1] + a[i + 2][j + 2];
-            if (sum > max)
-                max = sum;
+            if (j + 2 < 6 && i + 2 < 6)
+            {
+                int sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                if (sum > max)
+                    max = sum;
+            }
         }
     }
+
     cout << max;
+
     return 0;
 }
