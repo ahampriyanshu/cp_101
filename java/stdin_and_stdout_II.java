f9b08f5333cc8baabcd06b57b5387d5f8be8e29c
priyanshumay <tiwarimay2002@gmail.com>
2020-04-30 01:11:02 +0530

    java added

---
+++ java/stdin_and_stdout_II.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
+        int i = Integer.parseInt(scan.readLine());
+        double d = Double.parseDouble(scan.readLine());
+        String s = scan.readLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
