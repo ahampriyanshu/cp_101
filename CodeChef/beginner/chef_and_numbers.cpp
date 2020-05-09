2ff4091a9f9bbce5c8818884d8594408947ac51f
priyanshumay <tiwarimay2002@gmail.com>
2020-05-09 14:43:57 +0530

    codechef initiated

---
+++ CodeChef/beginner/chef_and_numbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int test,num,plate,i,j;
+cin >> test;
+for ( i=0; i<test ; i++ )
+{
+    cin >> num;
+    int arr[num];
+    for ( j=0 ; j<num ; j++ )
+    {
+       cin >>  arr[j];
+    }
+        for ( j=0 ; j<num ; j++ )
+    {
+        ;
+    }
+}
+
+	return 0;
+}
