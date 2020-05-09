eb24f0518195785c0c88816ee3931ae7b92d1720
priyanshumay <tiwarimay2002@gmail.com>
2020-05-09 15:07:04 +0530

    wierd shit

---
+++ CodeChef/easy/bring_up_your_own_horse.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int n;
+    cin>>n;
+    cout<<n*1<<endl<<n*2<<endl<<n*3<<endl<<n*4<<endl<<n*5<<endl<<n*6<<endl<<n*7<<endl<<n*8<<endl<<n*9<<endl<<n*10;
+    return 0;
+}
