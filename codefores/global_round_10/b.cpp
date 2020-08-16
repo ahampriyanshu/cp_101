c6ea70dec7f3c02f605ac3dcf89559510beaeaf0
priyanshumay <tiwarimay2002@gmail.com>
2020-08-16 23:27:31 +0530

    completed codeforces global round 10

---
+++ codefores/global_round_10/b.cpp
@@ -0,0 +1,65 @@
+/*
+Author : priyanshumay
+Problem: B
+Contest: 1392
+Platform: Codeforces
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+ll i, j, test, N, K, d;
+
+void solve()
+{
+    cin >> N >> K;
+    vector<ll> arr(N);
+    for (i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+
+    if (K & 1)
+    {
+    auto maximum = max_element(arr.begin(), arr.end());
+    d = *maximum;
+        for (i = 0; i < N; i++) 
+        {    
+        arr[i] = d - arr[i];
+        }
+    }
+    else
+    {
+        for (int j = 0; j <= 1 ; j++) {
+            
+        auto maximum = max_element(arr.begin(), arr.end());
+        d = *maximum;
+
+ 		for (i = 0; i < N; i++)
+        { 
+ 			arr[i]  = d - arr[i];
+ 			
+ 		}
+ 		
+ 	}
+    }
+    
+    for (int i = 0; i < N; i++)
+    { 
+    cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    for (cin >> test; test--;)
+    {
+        solve();
+        cout << "\n";
+    }
+}
---
+++ codefores/global_round_10/c.cpp
@@ -0,0 +1,52 @@
+/*
+Author : priyanshumay
+Problem: C
+Contest: 1392
+Platform: Codeforces
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+ll i, j, test, N;
+
+ll solve()
+{
+   		cin >> N;
+
+        ll ans[N];
+		vector<ll> arr(N);
+
+		for(i = 0; i < N; i++)
+        {
+			cin >> arr[i];
+		}
+
+		ans[0] = 0;
+        i = 1;
+
+		while(i<N)
+        {
+			ans[i] = ans[i-1];
+			if(arr[i] < arr[i-1]) 
+            {
+			ans[i] = ans[i] + arr[i-1] -arr[i];
+			}
+            i++;
+		}
+
+		return ans[N-1];
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    for (cin >> test; test--;)
+    {
+      cout << solve() << "\n";
+    }
+}
