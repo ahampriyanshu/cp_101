84e9ab6aeaa2d332d0f31fa8f275febffb7d0910
priyanshumay <tiwarimay2002@gmail.com>
2020-08-20 00:02:51 +0530

    completed codeforces code ensemble 2020

---
+++ codechef/external_rated_contest/priya_and_AND.cpp
@@ -0,0 +1,50 @@
+/*
+Author : priyanshumay
+Problem: CENS2020
+Contest: CENS20D
+Platform: codechef
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+int test, n;
+
+int solve(vector<int> ar, int N)
+{
+    int count = 0;
+
+    for (int i = 0; i < N - 1; ++i)
+    {
+        for (int j = i + 1; j < N; ++j)
+        {
+            if ( (ar[i] & ar[j]) == ar[i])
+            {
+                ++count;
+            }
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    for (cin >> test; test--;)
+    {
+        cin >> n;
+
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        cout << solve(arr, n) << "\n";
+    }
+}
